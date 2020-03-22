7bd7a4d4241b849518b8220e67d312dda15b6348
Abelrahman Reda <boodycat09@gmail.com>
2020-03-22 21:18:49 +0200

    add "insert" function without split option

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Data-Storage-Final-Project
-[![Build Status](http://localhost:8080/job/first-jenkins-job/badge/icon)](http://localhost:8080/job/first-jenkins-job/)
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -5,22 +6,25 @@ import java.io.RandomAccessFile;
 public class Main {
 
     static void CreateIndexFile(String fileName,int numberOfRecords,int m){
+        
         try {
+            int counter = 0 ;
             RandomAccessFile file = new RandomAccessFile(fileName,"rw");
             for (int i = 0; i <numberOfRecords ; i++) {
                 for (int j = 0; j <(m*2)+1 ; j++) {
                     if (j==1)
                     {
+                        counter++;
                         file.writeInt(i+1);
                     }
                     else
                     {
+                        counter++;
                         file.writeInt(-1);
                     }
                 }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -68,8 +72,10 @@ public class Main {
         }
         return 0;}
     static void DisplayIndexFileContent(String fileName){
-        try {
+              try {
             RandomAccessFile file = new RandomAccessFile(fileName,"rw");
+                  System.out.println("file length is "+file.length());
+            
             for (int i = 1; i <=file.length()/4 ; i++) {
                 System.out.print(" "+file.readInt());
                 if (i !=0 && i%11==0)
@@ -79,6 +85,7 @@ public class Main {
             e.printStackTrace();
         }
     }
+    
     public static void fill(String path){
         try {
             RandomAccessFile file= new RandomAccessFile(path,"rw");
@@ -92,27 +99,118 @@ public class Main {
                     0,24,60,30,196,32,240,-1,-1,-1,-1,
                     1,3,2,7,4,10,5,-1,-1,-1,-1,
                     1,15,3,19,6,32,7,-1,-1,-1,-1};
-
+                
             for (int i = 0; i <arr.length ; i++)
                 file.writeInt(arr[i]);
-            file.seek(0);
+    
+           
+            
+            
+         /*   file.seek(0);
             while (file.getFilePointer()!=(file.length()))
-                System.out.println(file.readInt());
+                System.out.println(file.readInt());*/
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
+    public static void inertNode(RandomAccessFile file,int key , int ref,int pointer){
+    
+        System.out.println("ref is "+pointer);
+           try {
+             
+               while ((int)file.getFilePointer()%44!=0)
+               {
+                  // System.out.println("@@@@@@"+file.readInt());
+                   int tempKey = file.readInt();    //
+                   int tempRef = file.readInt();    //
+                   System.out.println("tempKey "+tempKey);
+                   System.out.println("tempRef "+tempRef);
+                   file.seek(file.getFilePointer()-8); //step 8 step backward
+                   file.writeInt(key);
+                   file.writeInt(ref);
+                   key = tempKey;
+                   ref=tempRef;
+               }
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+    
+       }
+    
+    
+    public static void cordinateNode( int recordNumber, int key,int referece) throws FileNotFoundException {
+            RandomAccessFile file = new RandomAccessFile("src/index.bin","rw");
+        try {
+            file.seek((recordNumber*44));
+            int y = file.readInt();
+            if (y == -1)
+            {
+                file.seek(file.getFilePointer()-4);
+                file.writeInt(0);
+                System.out.println("firt insertion to the record");
+                file.writeInt(key);
+                file.writeInt(referece);
+            }
+            else {
+            while (true)
+            {
+             int x = file.readInt();
+                System.out.println("x is "+x+" with index ");
+             if (key<x)
+             {
+                 file.seek(file.getFilePointer()-4);
+                 inertNode(file,key,referece,(int)(file.getFilePointer()));
+                 break;
+             }
+             else if (x == -1)
+             {
+                 System.out.println("file.getFilePointer() ::"+file.getFilePointer());
+                 file.seek(file.getFilePointer()-4);
+                 file.writeInt(key);
+                 file.writeInt(referece);
+                 break;
+             }
+             else
+             {
+                 file.readInt();
+             }
+             
+            }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String args[])
     {
+        
         /*initialize project*/
         String indexFileName="src//index.bin";
-       // CreateIndexFile(indexFileName,10,5);
-       // DisplayIndexFileContent(indexFileName);
-        fill(indexFileName);
-        SearchARecord(indexFileName,5);
-        // CreateIndexFile(indexFileName,10,5);
+        CreateIndexFile(indexFileName,10,5);
+        DisplayIndexFileContent(indexFileName);
+        try {
+            cordinateNode(1,10,50);
+            cordinateNode(1,30,50);
+            cordinateNode(1,60,50);
+            cordinateNode(1,5,50);
+            cordinateNode(1,1,50);
+          //  cordinateNode(1,6,60);
+         //  cordinateNode(1,3,650);
+           // cordinateNode(1,2,650);
+          // cordinateNode(1,1,650);
+           // cordinateNode(1,1,650);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        DisplayIndexFileContent(indexFileName);
+    
+       
+        //fill(indexFileName);
+        //SearchARecord(indexFileName,5);
+        //CreateIndexFile(indexFileName,10,5);
         // DisplayIndexFileContent(indexFileName);
     }
 }
